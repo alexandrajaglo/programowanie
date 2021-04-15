58b3070a69ecc72874de4a4ebba1402eb668d817
alexandrajaglo <80705440+alexandrajaglo@users.noreply.github.com>
2021-04-15 08:04:20 +0200

    Update zestaw4_zad9.cpp

--- zestaw4_zad9.cpp
+++ zestaw4_zad9.cpp
@@ -2,16 +2,16 @@
 
 int main()
 {
-   int Sieve[200]={0};
-   for (int i=2;i<200;i++)
+   int Sieve[999]={0};
+   for (int i=2;i<999;i++)
     {
-      for (int j=i*i;j<200;j+=i)
+      for (int j=i*i;j<999;j+=i)
         {
          Sieve[j-1] = 1;
         }
    }
-   std::cout<<"Primes under 200: ";
-   for (int i=1;i<200;i++)
+   std::cout<<"Primes under 999: ";
+   for (int i=1;i<999;i++)
     {
       if (Sieve[i-1] == 0)
          std::cout<< i << ", ";
